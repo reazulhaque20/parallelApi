85a1f2ac0fa652c538296a3a01d5918d6f047516
Md. Reazul Haque <reazulhaque20@gmail.com>
2022-10-08 15:33:13 +0600

    asdf

---
+++ src/main/java/com/apex/parallelApi/ParallelApiApplication.java
@@ -0,0 +1,13 @@
+package com.apex.parallelApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/parallelApi/ParallelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.parallelApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
