3b4c3221d61cc1644add46f1bfc0f855c64d5143
Md. Reazul Haque <reazulhaque20@gmail.com>
2022-10-24 14:44:55 +0600

    123

--- src/main/java/com/apex/parallelApi/configuration/AsyncConfiguration.java
+++ src/main/java/com/apex/parallelApi/configuration/AsyncConfiguration.java
@@ -1,7 +1,9 @@
 package com.apex.parallelApi.configuration;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -9,14 +11,20 @@ import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfiguration  {
+    public class AsyncConfiguration implements AsyncConfigurer {
     @Bean
-    public Executor asyncExecutor() {
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(50);
-        executor.setQueueCapacity(20);
+        executor.setQueueCapacity(100);
+        executor.initialize();
         executor.setThreadNamePrefix("MyExecutor-");
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new CustomAsyncExceptionHandler();
     }
 }
